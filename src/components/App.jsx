37a287f2454aca18d405ae77454184d9b80d8489
Meria Thomas <josemeria93@gmail.com>
2018-11-13 22:44:39 -0800

    follow along react setup and installtion till eslint

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import TicketList from './TicketList';
+function App(){
+  return (
+    <div>
+      <Header/>
+      <TicketList/>
+      <hr/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TicketList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Ticket from './Ticket';
+function TicketList(){
+  return (
+    <Ticket
+      location="3A"
+      names="Thato an Haley"
+      issue="Firabase will not save records!" />
+  );
+}
+
+export default TicketList;
