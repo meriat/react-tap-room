63adbdfff5ca8a41f93c2c4161d12925fe342342
Meria Thomas <josemeria93@gmail.com>
2018-11-16 11:18:24 -0800

    complete header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,15 +9,27 @@ function Header(){
       text-align: center;
       margin-top: 4%;
       .jumbotron {
-        background-color: rgba(235,28,35, 0.4);
+        background-color: rgba(207,201,165, 0.5);
+        padding-top: 0px;
+        padding-bottom: 10px;
+      }
+      h1{
         font-family: 'Damion', cursive;
         font-weight: bold;
-        font-size:70px;
+        font-size:60px;
         color: black;
         text-shadow: 2px 2px 8px white;
+        padding-top: 0px;
+        margin: 0px;
+      }
+      img{
+        width: 8%;        
       }
       `} </style>
-      <h1 className="jumbotron">Cocorocco Bar</h1>
+      <div className="jumbotron">
+      <img src='https://melbournechapter.net/images/beer-tap-clipart-3.png'/>
+      <h1>Cocorocco Bar</h1>
+      </div>
       <Link className='btn btn-secondary' to='/'>Home</Link> | <Link className='btn btn-secondary'to='/beerList'>Beer List</Link>
     </div>
   );
