a05f7268b57c300147049886dcf2a962b3f4e785
Meria Thomas <josemeria93@gmail.com>
2018-12-02 16:09:04 -0800

    add styling to confirm component

--- src/components/ConfirmQuestions.jsx
+++ src/components/ConfirmQuestions.jsx
@@ -3,7 +3,20 @@ import PropTypes from 'prop-types';
 
 function ConfirmQuestions(props){
   return (
-    <div>
+    <div className='container'>
+      <style jsx>
+      {`
+      .container{
+        background-color: rgba(0,0,0,0.7);
+        box-shadow: 7px 0 50px 5px white;
+        color:#62f442;
+        margin-top: 60px;
+        text-align: center;
+        padding-top: 2.5em;
+        padding-bottom: 2.5em;
+      }
+      `}
+      </style>
       <p>Do you want to add a beer?</p>
       <button className='btn btn-success' onClick ={props.onTroubleshootingConfirmation}>Yes</button>
     </div>
